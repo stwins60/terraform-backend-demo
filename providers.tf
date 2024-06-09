2a69af5929fde5d705f0fc257590dd8f2be09846
Idris Fagbemi <idrisniyi94@gmail.com>
2024-06-09 17:17:06 -0500

    testing terraform pipline

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-fullstack-20241-terraform-state"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    profile = "dev"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "dev"
+}
